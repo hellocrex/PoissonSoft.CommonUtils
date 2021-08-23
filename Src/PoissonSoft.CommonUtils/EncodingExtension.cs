de65758540397b22f92c8aa03b9567e2d3b9f992
AaaBbb <sdgdfsgdf@sgds.com>
2021-08-23 22:12:58 +0300

    Helpers

---
+++ Src/PoissonSoft.CommonUtils/EncodingExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoissonSoft.CommonUtils
+{
+    /// <summary>
+    /// Encoding extensions
+    /// </summary>
+    public static class EncodingExtension
+    {
+        /// <summary>
+        /// Convert bytes to hex
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static string ToHex(this IEnumerable<byte> bytes)
+        {
+            string hex = string.Empty;
+            foreach (byte theByte in bytes)
+            {
+                hex += theByte.ToString("x2");
+            }
+            return hex;
+        }
+    }
+}
